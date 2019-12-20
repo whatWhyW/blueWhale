cb9ee58a1b355c0487b0204182ce260db5cd6221
v_wangyang16 <v_wangyang16@baidu.com>
2019-12-20 16:10:02 +0800

    添加前端node服务器 跨域配置 及上穿文件实现

--- server/app.js
+++ server/app.js
@@ -3,10 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,8 +16,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    if(req.method==="OPTIONS") res.sendStatus(200);/*让options请求快速返回*/
+    else next();
+});
+
+require('./router')(app);
+
+// upload start
+const upload = require('jquery-file-upload-middleware');
+// configure upload middleware
+upload.configure({
+    uploadDir: path.join(__dirname, 'public', 'uploads'),
+    uploadUrl: '/uploads',
+    imageVersions: {
+        thumbnail: {
+            width: 80,
+            height: 80
+        }
+    }
+});
+app.use('/upload', upload.fileHandler());
+// upload end
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,5 +58,32 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+upload.configure({
+    uploadDir: path.join(__dirname, 'public', 'uploads'),
+    uploadUrl: '/uploads',
+    imageVersions: {
+        thumbnail: {
+            width: 80,
+            height: 80
+        }
+    }
+});
+app.use('/upload', upload.fileHandler());
+// upload end
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 module.exports = app;
